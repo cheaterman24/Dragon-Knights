cee4b3d6ffee5dfde314b7006668db8defc1d3ab
cheaterman24 <anonymous2435@gmail.com>
2017-02-14 18:53:31 -0500

    Initial upload

---
+++ Assets/Scripts/CloudController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudController : MonoBehaviour {
+
+	public float R;
+	private Vector3 newpos;
+	public Renderer ren; 
+	public bool onCam = false;
+	private float scale;
+
+	// Use this for initialization
+	void Start () {
+		scale = Random.Range (0.5f, 1f);
+		R = scale / 20;
+		newpos = transform.position;
+		ren = gameObject.GetComponent<Renderer>();
+
+		if (scale > 0.75f) {
+			ren.sortingOrder = 0;
+		} else
+			ren.sortingOrder = 2;
+		
+		transform.localScale = new Vector3 (scale, scale, scale);
+	}
+
+	void OnBecameInvisible()
+	{
+		onCam =  true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		newpos.x += R;
+		transform.position = newpos;
+		if (onCam) {
+			Destroy (gameObject);
+			CloudGen.numLoaded--;
+			onCam = false;
+		}
+
+	
+	}
+}
---
+++ Assets/Scripts/CloudGen.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CloudGen : MonoBehaviour {
+
+	public static List <GameObject> loaded = new List<GameObject>();
+	private static Object[] cloudObjs;
+	public static int numLoaded = 0;
+
+	// Use this for initialization
+	void Start () {
+
+		cloudObjs = Resources.LoadAll ("Prefabs", typeof(GameObject));
+		if (numLoaded < 4) {
+			loaded.Add ((GameObject)cloudObjs [Random.Range (0, cloudObjs.Length - 1)]);
+			Instantiate (loaded [loaded.Count - 1],transform.position = new Vector3 (Random.Range(-40f,-25f), Random.Range(-10f,10f), Random.Range(-1f,0f)), Quaternion.identity);
+			numLoaded++;
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// take random cloud prefab from cloudobjs array and add to list of loaded clouds
+		if (numLoaded < 4) {
+			loaded.Add ((GameObject)cloudObjs [Random.Range (0, cloudObjs.Length - 1)]);
+			Instantiate (loaded [loaded.Count - 1],transform.position = new Vector3 (Random.Range(-40f,-25f), Random.Range(-10f,10f), Random.Range(-1f,0f)), Quaternion.identity);
+			numLoaded++;
+		}
+			
+	}
+}
